5f5c7afffbb655c772b999e3ecf418dfab282cfa
Steve Eicherly <steve.eicherly@cybergrx.com>
2020-01-14 12:14:53 -0700

    constants for challenge and authState

--- src/authentication/AuthState.js
+++ src/authentication/AuthState.js
@@ -1,7 +1,15 @@
 export const AuthState = {
+  loading: 'loading',
+  // Setting up MFA
+  TOTPSetup: 'TOTPSetup',
   // Needs MFA
   confirmSignIn: 'confirmSignIn',
+  forgotPassword: 'forgotPassword',
+  requireNewPassword: 'requireNewPassword',
   signIn: 'signIn',
   // Logged in
   signedIn: 'signedIn',
+  signedOut: 'signedOut',
+  signedUp: 'signedUp',
+  verifyContact: 'verifyContact',
 };
