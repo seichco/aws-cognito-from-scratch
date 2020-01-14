bdb92760904b7c8c40ab42b9c28fd224aff478b6
Steve Eicherly <steve.eicherly@cybergrx.com>
2020-01-14 14:43:07 -0700

    App component tree

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React from 'react';
 import { Authenticator } from 'aws-amplify-react';
+import React from 'react';
 import './App.css';
-import { SignIn } from './authentication/SignIn';
-import { ConfirmTotp } from './authentication/ConfirmTotp';
-import { LogOut } from './authentication/LogOut';
 import { AuthStatus } from './authentication/AuthStatus';
+import { ConfirmTotp } from './authentication/ConfirmTotp';
+import { ForgotPassword } from './authentication/ForgotPassword';
+import { Loading } from './authentication/Loading';
+import { NewPassword } from './authentication/NewPassword';
+import { SetupTotp } from './authentication/SetupTotp';
+import { SignIn } from './authentication/SignIn';
+import { SignOut } from './authentication/SignOut';
+import { StatePicker } from './authentication/StatePicker';
+import { VerifyContact } from './authentication/VerifyContact';
 
 function App() {
   return (
     <Authenticator hideDefault>
+      <StatePicker />
+      <Loading />
       <SignIn />
-      <LogOut />
+      <ForgotPassword />
+      <NewPassword />
+      <SetupTotp />
       <ConfirmTotp />
+      <VerifyContact />
+      <SignOut />
       <AuthStatus />
     </Authenticator>
   );
